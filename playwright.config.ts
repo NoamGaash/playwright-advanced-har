53c04e317e4bfeb2b235905f5b37ac166e735d61
Noam Gaash <noam.gaash@applitools.com>
2023-10-20 22:36:47 +0300

    fix: linter definition

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
@@ -10,19 +10,19 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
-  use: {
-    trace: 'on-first-retry',
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-  ]
+	testDir: "./tests",
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 2 : 0,
+	workers: process.env.CI ? 1 : undefined,
+	reporter: "html",
+	use: {
+		trace: "on-first-retry",
+	},
+	projects: [
+		{
+			name: "chromium",
+			use: { ...devices["Desktop Chrome"] },
+		},
+	],
 });
--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,37 +1,37 @@
-import { expect, type Page } from "@playwright/test";
+import { expect } from "@playwright/test";
 import { test } from "../src/index";
 import fs from "fs";
 
 test("sanity", async ({ page }) => {
-  await page.routeFromHAR("tests/har/demo-todo-app.har");
-  await page.goto("https://demo.playwright.dev/todomvc");
+	await page.routeFromHAR("tests/har/demo-todo-app.har");
+	await page.goto("https://demo.playwright.dev/todomvc");
 });
 
 test("sanity with option", async ({ page }) => {
-  await page.routeFromHAR("tests/har/demo-todo-app.har", {
-    update: false,
-    updateContent: "embed",
-  });
-  await page.goto("https://demo.playwright.dev/todomvc");
+	await page.routeFromHAR("tests/har/demo-todo-app.har", {
+		update: false,
+		updateContent: "embed",
+	});
+	await page.goto("https://demo.playwright.dev/todomvc");
 });
 
 test("record", async ({ page }) => {
-  await page.routeFromHAR("tests/har/temp-record.har", {
-    update: true,
-    updateContent: "embed",
-  });
-  await page.goto("https://demo.playwright.dev/todomvc");
-  await page.close();
-  await page.context().close();
-  await page.context().browser()?.close();
-  const data = await fs.promises.readFile("tests/har/temp-record.har", { encoding: "utf8" });
-  const har = JSON.parse(data);
-  expect(har.log.entries.length).toBeGreaterThan(0);
-  expect(har.log.entries[0].request.url).toBe("https://demo.playwright.dev/todomvc");
-  expect(har.log.entries[0].response.status).toBeGreaterThanOrEqual(200);
-  expect(har.log.entries[0].response.status).toBeLessThan(400);
+	await page.routeFromHAR("tests/har/temp-record.har", {
+		update: true,
+		updateContent: "embed",
+	});
+	await page.goto("https://demo.playwright.dev/todomvc");
+	await page.close();
+	await page.context().close();
+	await page.context().browser()?.close();
+	const data = await fs.promises.readFile("tests/har/temp-record.har", { encoding: "utf8" });
+	const har = JSON.parse(data);
+	expect(har.log.entries.length).toBeGreaterThan(0);
+	expect(har.log.entries[0].request.url).toBe("https://demo.playwright.dev/todomvc");
+	expect(har.log.entries[0].response.status).toBeGreaterThanOrEqual(200);
+	expect(har.log.entries[0].response.status).toBeLessThan(400);
 
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  // clean up
-  await fs.promises.rm("tests/har/temp-record.har");
+	await new Promise((resolve) => setTimeout(resolve, 1000));
+	// clean up
+	await fs.promises.rm("tests/har/temp-record.har");
 });
