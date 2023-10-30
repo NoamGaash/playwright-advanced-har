35e21d5de66008fde74d9ec9b1419f85e9f196ac
Noam Gaash <noam.gaash@applitools.com>
2023-10-30 16:54:57 +0200

    test: customMatcher

--- src/utils/matchers/customMatcher.ts
+++ src/utils/matchers/customMatcher.ts
@@ -4,13 +4,8 @@ import { scoreByHeaders, jsonEquals } from "./comparators";
 /** 
     @experimental
 */
-export const customMatcher: CustomMatcher = ({
-	methodComparator = (a, b) => a === b,
-	urlComparator = (a, b) => a === b,
-	postDataComparator = {
-		POST: jsonEquals,
-	},
-}) => {
+export const customMatcher: CustomMatcher = (options = defaultArgs) => {
+	const { methodComparator, urlComparator, postDataComparator, scoring } = { ...defaultArgs, ...options };
 	const compare: Matcher = (request, entry) => {
 		if (!methodComparator(request.method(), entry.request.method)) return -1;
 		if (!urlComparator(request.url(), entry.request.url)) return -1;
@@ -19,17 +14,27 @@ export const customMatcher: CustomMatcher = ({
 				? postDataComparator
 				: postDataComparator?.[entry.request.method as Method];
 		if (bodyCompare && !bodyCompare(request.postData() ?? "{}", entry.request.postData?.text ?? "{}")) return -1;
-		return scoreByHeaders(request.headers(), entry.request.headers);
+		return scoring(request, entry);
 	};
 	return compare;
 };
 
-export type CustomMatcher = (options: {
-	methodComparator?: (a: string, b: string) => boolean;
-	urlComparator?: (a: string, b: string) => boolean;
-	postDataComparator?:
+const defaultArgs: {
+	methodComparator: (a: string, b: string) => boolean;
+	urlComparator: (a: string, b: string) => boolean;
+	postDataComparator:
 		| Partial<{
 				[method in Method]: (a: string, b: string) => boolean;
 		  }>
 		| ((a: string, b: string) => boolean);
-}) => Matcher;
+	scoring: Matcher;
+} = {
+	methodComparator: (a, b) => a === b,
+	urlComparator: (a, b) => a === b,
+	postDataComparator: {
+		POST: jsonEquals,
+	},
+	scoring: (request, entry) => scoreByHeaders(request.headers(), entry.request.headers),
+};
+
+export type CustomMatcher = (options?: Partial<typeof defaultArgs>) => Matcher;
---
+++ tests/customMatchers.spec.ts
@@ -0,0 +1,121 @@
+import { expect } from "@playwright/test";
+import { defaultMatcher, customMatcher, test } from "../lib/index";
+import fs from "fs";
+
+test("sanity", async ({ page, advancedRouteFromHAR }) => {
+	await advancedRouteFromHAR("tests/har/demo-todo-app.har", { matcher: customMatcher() });
+	await page.goto("https://demo.playwright.dev/todomvc");
+});
+
+test("sanity with matcher", async ({ page, advancedRouteFromHAR }) => {
+	await advancedRouteFromHAR("tests/har/demo-todo-app.har", {
+		update: false,
+		updateContent: "embed",
+		matcher: customMatcher({
+			scoring: () => 1,
+		}),
+	});
+	await page.goto("https://demo.playwright.dev/todomvc");
+});
+
+test("ignore 500 errors", async ({ page, advancedRouteFromHAR }) => {
+	await advancedRouteFromHAR("tests/har/first-has-error.har", {
+		matcher: customMatcher({
+			scoring(request, entry) {
+				if (entry.response.status === 500) {
+					return -1;
+				}
+				return defaultMatcher(request, entry);
+			},
+		}),
+	});
+	page.on("response", (response) => {
+		expect(response.status()).toBeLessThan(500);
+	});
+	await page.goto("https://noam-gaash.co.il");
+	await page.getByText("Hello World").waitFor();
+});
+
+test("get 500 errors", async ({ page, advancedRouteFromHAR }) => {
+	await advancedRouteFromHAR("tests/har/first-has-error.har", {
+		matcher: customMatcher({
+			scoring(request, entry) {
+				if (entry.response.status !== 500) {
+					return -1;
+				}
+				return defaultMatcher(request, entry);
+			},
+		}),
+	});
+	let gotError = false;
+	page.on("response", (response) => {
+		expect(response.status()).toBe(500);
+		gotError = true;
+	});
+	await page.goto("https://noam-gaash.co.il");
+	expect(gotError).toBe(true);
+});
+
+test("largest number", async ({ page, advancedRouteFromHAR }) => {
+	// the file contains 3 responses - 42, 1234, 5
+	await advancedRouteFromHAR("tests/har/differentNumbers.har", {
+		matcher: customMatcher({
+			scoring(request, entry) {
+				return Number(entry.response.content.text);
+			},
+		}),
+	});
+	await page.goto("https://noam-gaash.co.il");
+	await page.getByText("1234").waitFor();
+});
+
+test("ignore port number", async ({ page, advancedRouteFromHAR }) => {
+	await advancedRouteFromHAR("tests/har/different-port.har", {
+		matcher: customMatcher({
+			urlComparator(a, b) {
+				a = a.replace(/:\d+/, "");
+				b = b.replace(/:\d+/, "");
+				return a === b;
+			},
+		}),
+	});
+	await page.goto("https://noam-gaash.co.il");
+	await page.getByText("from different port").waitFor();
+});
+
+test("ignore search params", async ({ page, advancedRouteFromHAR }) => {
+	await advancedRouteFromHAR("tests/har/different-search-params.har", {
+		matcher: customMatcher({
+			urlComparator(a, b) {
+				a = a.replace(/\?.*/, "");
+				b = b.replace(/\?.*/, "");
+				return a === b;
+			},
+		}),
+	});
+	await page.goto("https://noam-gaash.co.il?search=1");
+	await page.getByText("from different search params").waitFor();
+});
+
+test("pick arbirtrary response", async ({ page, advancedRouteFromHAR }) => {
+	// good when you're testing a long polling requests
+	await advancedRouteFromHAR("tests/har/differentNumbers.har", {
+		matcher: customMatcher({
+			scoring() {
+				return Math.random();
+			},
+		}),
+	});
+	await page.goto("https://noam-gaash.co.il");
+	let attempts = 0;
+	for (const waitfor of ["1234", "42", "5"]) {
+		while ((await page.getByText(waitfor).count()) === 0) {
+			attempts++;
+			await page.reload();
+			if (attempts > 100) {
+				// the probability of not getting the right response after 100 attempts is 2/3^100 = 3.8e-48
+				throw new Error("too many attempts");
+			}
+		}
+	}
+});
