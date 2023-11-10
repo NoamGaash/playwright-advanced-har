e448c019ca5bf91f6916280571d4b102ff6e1d22
Noam Gaash <noam.gaash@applitools.com>
2023-11-10 18:05:35 +0200

    fix: lint

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,11 @@ export const test = base.extend<{
 		const advancedRouteFromHAR: AdvancedRouteFromHAR = async (filename, options) => {
 			if (options?.update) {
 				// on update, we want to record the HAR just like the original playwright method
-				return originalRouteFromHAR(filename, options);
+				return originalRouteFromHAR(filename, {
+					update: true,
+					updateContent: options?.updateContent,
+					updateMode: options?.updateMode,
+				});
 			} else {
 				const har = JSON.parse(await fs.promises.readFile(filename, { encoding: "utf8" }));
 				return serveFromHar(
